2dfb0f18d923edd72a806f19e326bb3336a6a5ea
Magnetar <contact@magnetar.dev>
2022-08-08 22:15:35 -0400

    feat: first reader test

--- test/reader.test.ts
+++ test/reader.test.ts
@@ -1,10 +1,23 @@
-import { assert, expect, test } from "vitest";
-import { createReader, createWriter } from "../src";
+import { readFile } from "node:fs/promises";
+import { fileURLToPath } from "node:url";
+import { expect, test } from "vitest";
+import { createReader } from "../src";
 
-test("zip", () => {
-	expect(2).toBe(2);
-});
+function loadArchive(file: string): Promise<ArrayBuffer> {
+	let url = new URL(file, import.meta.url);
+	let path = fileURLToPath(url);
+	return readFile(path);
+}
 
-test("7z", () => {
-	expect(0).toBe(2);
-});
+test("zip", async () => {
+	const buffer = await loadArchive("./examples/test.zip");
+	const reader = await createReader(buffer);
+	let fileData: Uint8Array | undefined;
+	for (const file of reader) {
+		fileData = file.extract();
+	}
+	reader.close();
+	expect(fileData, "file data to exist").toBeDefined();
+	let text = new TextDecoder().decode(fileData!);
+	expect(text, "file data to be correct").toBe("test\n");
+}, 60_000);
